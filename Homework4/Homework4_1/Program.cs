07d9d24db38c3de4c7c67456521ce050674a6e63
Asimov6680 <79745114+Asimov6680@users.noreply.github.com>
2021-03-20 15:43:00 +0800

    Add files via upload

---
+++ Homework4/Homework4_1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_1
+{
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+
+            GenericList<int> gl = new GenericList<int>();
+
+            gl.Add(3);
+            gl.Add(4);
+            gl.Add(5);
+
+
+            Console.Write("元素分别为：");
+            gl.ForEach(nodeData => Console.Write(nodeData+" "));
+            gl.ForEach(nodeData => sum += nodeData);
+            gl.ForEach(nodeData =>
+            {
+                if (nodeData > max)
+                {
+                    max = nodeData;
+                }
+                if (nodeData < min)
+                {
+                    min = nodeData;
+                }
+            });
+
+            Console.WriteLine();
+            Console.WriteLine("总和是："+sum);
+            Console.WriteLine("最小值是："+min);
+            Console.WriteLine("最大值是："+max);
+        }
+    }
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_2
+{
+
+    public delegate void TickHandler(object sender, TickEventArgs args);
+
+    public class TickEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public int SetHour { get; set; }
+        public int SetMinute { get; set; }
+        public int SetSecond { get; set; }
+
+        public static string AddZero(int num)
+        {
+            return num < 10 ? "0" : "";
+        }
+    }
+
+    public class Clock
+    {
+        public event TickHandler OnTick;
+        public event TickHandler OnAlarm;
+        private TickEventArgs args=new TickEventArgs();
+
+        public void SetClock(int hour, int minute, int second)
+        {
+            args.Hour = hour;
+            args.Minute = minute;
+            args.Second = second;
+        }
+
+        public void SetAlarm(int setHour, int setMinute, int setSecond)
+        {
+            args.SetHour = setHour;
+            args.SetMinute = setMinute;
+            args.SetSecond = setSecond;
+        }
+        public void Tick()
+        {
+            int h = args.Hour;
+            int m = args.Minute;
+            int s = args.Second;
+
+            Console.WriteLine($"tick starts on {TickEventArgs.AddZero(h)}{h}:{TickEventArgs.AddZero(m)}{m}:{TickEventArgs.AddZero(s)}{s}");
+
+            while (true)
+            {
+
+                System.Threading.Thread.Sleep(10);
+
+                if (args.Second >= 59)
+                {
+                    args.Second = 0;
+                }
+                else
+                {
+                    args.Second++;
+                }
+
+                if (args.Second == 0)
+                {
+                    if (args.Minute < 59)
+                    {
+                        args.Minute++;
+                    }
+                    else
+                    {
+                        args.Minute = 0;
+                    }
+                }
+
+                if (args.Minute == 0 &&args.Second==0)
+                {
+                    if (args.Hour < 23)
+                    {
+                        args.Hour++;
+                    }
+                    else
+                    {
+                        args.Hour = 0;
+                    }
+                }
+
+                OnTick(this,args);
+                if (args.Hour == args.SetHour && args.Minute == args.SetMinute && args.Second == args.SetSecond)
+                {
+                    OnAlarm(this, args);
+                }
+            }
+        }
+    }
+
+    public class MyClock
+    {
+        public Clock clock1 = new Clock();
+
+        public MyClock(int hour,int minute,int second)
+        {
+            clock1.SetClock(hour, minute, second);
+            clock1.OnTick += Clock_Tick1;
+            clock1.OnAlarm += Clock_Alarm1;
+            
+        }
+
+        void Clock_Tick1(object sender,TickEventArgs args)
+        {
+            int s = args.Second;
+            int m = args.Minute;
+            int h = args.Hour;
+            Console.WriteLine($"tick  {TickEventArgs.AddZero(h)}{h}:{TickEventArgs.AddZero(m)}{m}:{TickEventArgs.AddZero(s)}{s}");
+                
+        }
+
+        void Clock_Alarm1(object sender,TickEventArgs args)
+        {
+            int s = args.Second;
+            int m = args.Minute;
+            int h = args.Hour;
+            Console.WriteLine($"ALARM  {TickEventArgs.AddZero(h)}{h}:{TickEventArgs.AddZero(m)}{m}:{TickEventArgs.AddZero(s)}{s}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int startHour = 23, startMinute = 59, startSecond = 55;
+            MyClock mc = new MyClock(startHour, startMinute, startSecond);
+
+            int setHour = 0, setMinute = 0, setSecond =8 ;
+            mc.clock1.SetAlarm(setHour, setMinute, setSecond);
+
+            mc.clock1.Tick();
+           
+        }
+    }
+}
