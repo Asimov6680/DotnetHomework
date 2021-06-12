1c09791afaf1b3486dd682a0607426b0b0093a7d
Asimov6680 <79745114+Asimov6680@users.noreply.github.com>
2021-06-12 21:01:12 +0800

    Add files via upload

---
+++ homework12/Models/OrdersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Homework5_1;
+using Homework6;
+using Microsoft.EntityFrameworkCore;
+
+namespace homework12.Models
+{
+    public class OrdersContext:DbContext
+    {
+        public OrdersContext(DbContextOptions<OrdersContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
